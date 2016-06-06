528fdeef4cbc2a95b1f4eae90b2c524ac07c4708
Matheus Mariano <vmatheus.mariano@gmail.com>
2016-06-06 19:39:01 -0300

    Add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  def index
+    render :json => User.all
+  end
+
+  def show
+    render :json => User.find(params[:id])
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    validate_and_render
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    validate_and_render
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    render json: { deleted: true }
+  end
+
+  private
+
+  def validate_and_render
+    if @user.valid?
+      render :json => @user
+    else
+      render :json => @user.errors.messages, status: 400
+    end
+  end
+
+  def user_params
+    params.permit(:name, :email, :sex, :birthday)
+  end
+end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get show" do
+    user = users(:matheus)
+    get :show, id: user.id
+    assert_response :success
+  end
+
+  test "should create new user" do
+    post :create, { name: "Marie", email: "marie@me.com", sex: "female", birthday: "1991-07-25" }
+    assert_response :success
+    assert_not_nil assigns(:user).id
+  end
+
+  test "should not create new user with invalid parameters" do
+    post :create, { name: "Marie", email: "invalidemail.com", sex: "female" }
+    assert_not assigns(:user).valid?
+    assert_response 400
+  end
+
+  test "should edit existing user" do
+    user = users(:matheus)
+    patch :update, { id: user.id, email: "vmatheus.mariano@gmail.com" }
+    assert_response :success
+    assert_equal "vmatheus.mariano@gmail.com", assigns(:user).email
+  end
+
+  test "should not edit user with invalid parameters" do
+    user = users(:matheus)
+    patch :update, { id: user.id, email: "invalidemail.com" }
+    assert_response 400
+  end
+
+  test "should delete existing user" do
+    user = users(:matheus)
+    delete :destroy, { id: user.id }
+    assert_response :success
+  end
+end
