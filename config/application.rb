6f4b69b136ad7c47d6762aa26ef1c924b12a727d
Matheus Mariano <vmatheus.mariano@gmail.com>
2016-06-07 15:57:09 -0300

    Add CORS

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module Apiservice
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins 'http://localhost:4200'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch]
+      end
+    end
   end
 end
